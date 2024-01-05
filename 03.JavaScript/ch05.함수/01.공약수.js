61ad20d73cde5bdac6297020dc61965a645eb9f4
songhaetnim <minshinzz@naver.com>
2024-01-05 12:22:26 +0900

    완전수

--- 03.JavaScript/ch05.함수/01.공약수.js
+++ 03.JavaScript/ch05.함수/01.공약수.js
@@ -1,1 +1,16 @@
-//
+// 함수 사용의 필요성
+function getDivisors(num){ //getDivisors()결과를 리턴? 알려주는 함수
+    let divisors = [];
+    for (let i = 1; i <= num; i++){
+        if (num % i == 0)
+            divisors.add(i);
+        }
+        return divisors;
+}
+
+let divisors1 = getDivisors(12);
+let divisors2 = getDivisors(30);
+console.log(divisors1);
+console.log(divisors2);
+
+
