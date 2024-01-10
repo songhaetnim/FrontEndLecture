6037845393296d94b97189e1ecb0bdb42d3e0f7c
songhaetnim <minshinzz@naver.com>
2024-01-10 18:37:30 +0900

    공부

--- 03.JavaScript/ch02.기본/03.자료형.js
+++ 03.JavaScript/ch02.기본/03.자료형.js
@@ -2,7 +2,7 @@
 // 1. Number
 console.log(5 + 2, 5 - 2 , 5 * 2, 5 / 2, ); // +, -, * , /
 console.log(5 % 2);     // modulo, 나머지
-console.log(2 ** 10);   // 2의 10제곱, 1024(1k)
+console.log(2 ** 10);   // 2의 10제곱, 1024(1k) ***X2
 console.log(1.3e-8);   // 1.3 * 10 ** -8
 
 // 2. 문자열(stiong)
@@ -25,11 +25,14 @@ console.log(`올해는 ${new Date().getFullYear()}년입니다.`);
 
 // 3. 논리형(boolean)
 console.log(typeof(true), typeof(false));
-console.log(2 == 2.0, 5 >= 4, !('가나다' > '마바사')); // 관계 연산자 ! = not
+console.log(2 == 2.0, 5 >= 4, !('가나다' > '마바사')); // 관계 연산자   ?= true ,진실,참,   ! = not 거짓(또는 반대)
 
 let x = 10 ;
-console.log(x > 8 || x < -3);   // 논리 연산자 ||(or), x > 8 또는 x < -3
-console.log(x > 8 && x < 12);   // && (and), 8 < x < 12
+console.log(x > 8 || x < -3);   // 논리 연산자 ||(or,또는 둘중에 하나라도 맞으면 된다.), x > 8 또는 x < -3 
+console.log(x > 8 && x < 12);   // && (and 둘다 똑같아야한다.), 8 < x < 12
 console.log(true > 10); // true=1, false=0 값이 있으면 true 0이거나 값이 없으면 false
+
+
+
 
 
--- 03.JavaScript/ch02.기본/04.변수.js
+++ 03.JavaScript/ch02.기본/04.변수.js
@@ -8,7 +8,7 @@
 //      7. 클래스 명은 대문자로 시작
 
 // 변수(variable)
-let pi = 3.1415926535;  // 신숙
+let pi = 3.1415926535;  // 신식
 var radius = 10;        // 구식
 console.log(`둘레는 ${2 * pi * radius}, 면적은${pi * radius * radius} 입니다.`);
 
@@ -21,24 +21,24 @@ console.log(a, str)
 
 // 증감 연산자
 let x = 0, y = 0;
-console.log(x++, ++y);      // post-increment, pre-increment
+console.log(x++, ++y);      // post-increment, pre-increment ++x는 지금 당장 1올라가는거   x++는 나중에 1올라가는거  ++앞에붙이는거와 뒤에붙이는건 차이남.
 console.log(x, y);
-console.log(x--, --y);      
+console.log(x--, --y);      // x--나중에 1감소하는거   --x지금 당장 1감소하는거   -- 앞에붙는거와 뒤에 붙는게 차이남
 console.log(x, y);
 
 // 나머지 자료형
 // 4. 함수(function)
 console.log(typeof function() { });
-console.log(typeof(() => {}));      // 화살표(arrow) 함수, 람다 함수, 익명 함수
+console.log(typeof(() => {}));      // 화살표(arrow) 함수, 람다 함수, 익명 함수 //typeof= 타입을 알려주는 태그다 함수인지,문자열인지,넘버인지 알려주는 단어
 
 // 5. 객체(object)
 let obj = {x: 1, y: 2};
 console.log(typeof obj);
-console.log(obj.x, obj.y);
+console.log(obj.x, obj.y);붙고
 
 // 6. undefined
 let alpha;
-console.log(typeof alpha, typeof beta);     // 초기화하지 않은 변수, 선언하지 않은 변수
+console.log(typeof alpha, typeof beta);     // 초기화하지 않은 변수, 선언하지 않은 변수,의미없는 변수(요소)
 alpha = 123;
 beta = 456;
 console.log(typeof alpha, typeof beta);
