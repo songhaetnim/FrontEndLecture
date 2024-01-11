3c7101d63916dc2635a9e312b7d20deb2e374672
songhaetnim <minshinzz@naver.com>
2024-01-11 19:10:28 +0900

    부트스트랩

--- 03.JavaScript/ch02.기본/03.자료형.js
+++ 03.JavaScript/ch02.기본/03.자료형.js
@@ -2,7 +2,7 @@
 // 1. Number
 console.log(5 + 2, 5 - 2 , 5 * 2, 5 / 2, ); // +, -, * , /
 console.log(5 % 2);     // modulo, 나머지
-console.log(2 ** 10);   // 2의 10제곱, 1024(1k) ***X2
+console.log(2 ** 10);   // 2의 10제곱, 1024(1k) ***X2ㄴㄴ
 console.log(1.3e-8);   // 1.3 * 10 ** -8
 
 // 2. 문자열(stiong)
@@ -31,8 +31,3 @@ let x = 10 ;
 console.log(x > 8 || x < -3);   // 논리 연산자 ||(or,또는 둘중에 하나라도 맞으면 된다.), x > 8 또는 x < -3 
 console.log(x > 8 && x < 12);   // && (and 둘다 똑같아야한다.), 8 < x < 12
 console.log(true > 10); // true=1, false=0 값이 있으면 true 0이거나 값이 없으면 false
-
-
-
-
-
